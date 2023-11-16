fdbd51534ed4e85e4df95f4ad5583c1de93cebb5
Leidy Castaño <leidy.castanoc@udea.edu.co>
2023-11-16 13:16:37 -0500

    Gestion de usuarios

--- components/NavigationCard.tsx
+++ components/NavigationCard.tsx
@@ -1,19 +1,20 @@
+import Link from "next/link";
+
 interface NavigationCardProps {
   title: string;
-  description: string;
   href: string;
 }
 
-const NavigationCard = ({ title, description, href }: NavigationCardProps) => {
+const NavigationCard = ({ title, href }: NavigationCardProps) => {
   return (
-    <div className="navigation-card">
-      <a href={href}>
-        <div className="flex flex-col items-center">
+    <div className="navigation-card border border-gray-300 gap-2  p-4 w-full h-314">
+      <Link href={href}>
+        <div className="flex flex-col px-2 font-semibold">
           <h3>{title}</h3>
-          <span>{description}</span>
         </div>
-      </a>
+      </Link>
     </div>
   );
 };
+
 export { NavigationCard };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import { NavigationCard } from "@/components/NavigationCard";
+
 const Home = () => {
-  return <main className='h-screen w-full flex items-center justify-center'>Hola mundo</main>;
+  return (
+    <main className="flex flex-col h-screen w-full items-center justify-center">
+      <h1>Sistema de gestión de inventarios</h1>
+      <div className="mt-5">
+        <NavigationCard
+          title="Usuarios"
+          description="Gestionar los usuarios de la plataforma"
+          href="/usuarios"
+        />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
