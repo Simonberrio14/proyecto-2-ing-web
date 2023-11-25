55c7bb44ff366fbdfe74ae2d1f1cc75e94bed778
YulyAR1 <118707998+YulyAR1@users.noreply.github.com>
2023-11-24 23:07:32 -0500

    Revert "Actualización Sidebar Cambio de estilo pagina activa"

--- components/NavigationCard.tsx
+++ components/NavigationCard.tsx
@@ -3,24 +3,11 @@ import Link from "next/link";
 interface NavigationCardProps {
   title: string;
   href: string;
-  isSelected?: boolean;
-  onClick?: () => void;
 }
 
-const NavigationCard = ({ title, href, isSelected, onClick }: NavigationCardProps) => {
-  const handleClick = () => {
-    if (onClick) {
-      onClick();
-    }
-  };
-
+const NavigationCard = ({ title, href }: NavigationCardProps) => {
   return (
-    <div
-      onClick={handleClick}
-      className={`${
-        isSelected ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-800"
-      } navigation-card border border-gray-300 w-full h-314 cursor-pointer`}
-    >
+    <div className="navigation-card border border-gray-300 w-full h-314">
       <Link href={href}>
         <div className="flex flex-col items-center font-semibold">
           <h3>{title}</h3>
